ad137aa13529f6ba4a4e72c729c04f2764da4c86
y-lin-spice <53523049+y-lin-spice@users.noreply.github.com>
2022-04-25 16:24:52 +0900

    20220425

--- src/hooks/useAddTodos.ts
+++ src/hooks/useAddTodos.ts
@@ -12,46 +12,53 @@ export const useAddTodos = () => {
   const [createTodo] = useMutation<CreateTodoResultType, CreateTodoVariables>(
     CREATE_TODO
   );
+  // const prevTodos = apolloClient.readQuery<GetTodosResultType>({
+  //   query: GET_TODOS
+  // });
+
   return async (data: Pick<Todo, "title" | "tags">) => {
     return await createTodo({
       variables: { title: data.title, completed: false }
     }).then((result) => {
-      // TODO: 帰ってくる作ったTODOをキャッシュに書き込む
-      // if(result.data && result.data.createTodo) {
+      // TODO: 帰ってくるTODOをキャッシュに書き込む
+      // if (result.data && result.data.createTodo) {
       //   apolloClient.writeQuery({
       //     query: GET_TODOS,
       //     data: {
-      //       __typename: "Todos",
-      //       todos: [
-      //         ...(prevTodos?.todos || []),
-      //         {
-      //           __typename: "Todo",
-      //           id: Math.floor(Math.random() * 1000),
-      //           completed: false,
-      //           ...data
-      //         }
-      //       ]
+      //       __typename: "TodosPage",
+      //       todos: {
+      //         data: [
+      //           ...(prevTodos?.todos.data || []),
+      //           {
+      //             __typename: "Todo",
+      //             id: Math.floor(Math.random() * 1000),
+      //             completed: false,
+      //             ...data
+      //           }
+      //         ]
+      //       }
       //     }
+      //   });
+      //   apolloClient.readQuery({
+      //     query: GET_TODOS
+      //   });
       // }
+      // console.log(prevTodos);
+      // apolloClient.writeQuery({
+      //   query: GET_TODOS,
+      //   data: {
+      //     __typename: "Todos",
+      //     todos: [
+      //       ...(prevTodos?.todos || []),
+      //       {
+      //         __typename: "Todo",
+      //         id: Math.floor(Math.random() * 1000),
+      //         completed: false,
+      //         ...data
+      //       }
+      //     ]
+      //   }
+      // });
     });
-    // const prevTodos = apolloClient.readQuery<GetTodosResultType>({
-    //   query: GET_TODOS
-    // });
-    // console.log(prevTodos);
-    // apolloClient.writeQuery({
-    //   query: GET_TODOS,
-    //   data: {
-    //     __typename: "Todos",
-    //     todos: [
-    //       ...(prevTodos?.todos || []),
-    //       {
-    //         __typename: "Todo",
-    //         id: Math.floor(Math.random() * 1000),
-    //         completed: false,
-    //         ...data
-    //       }
-    //     ]
-    //   }
-    // });
   };
 };
